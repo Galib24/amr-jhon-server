6c853470190397f07db1a50688255c4fdf1c2f53
SQLAdmin <abuyeahia24@gmail.com>
2023-05-17 12:05:04 +0600

    added pagination

--- index.js
+++ index.js
@@ -39,7 +39,11 @@ async function run() {
 
 
         app.get('/products', async (req, res) => {
-            const result = await productCollection.find().toArray();
+            console.log(req.query);
+            const page = parseInt(req.query.page) || 0;
+            const limit = parseInt(req.query.limit) || 10;
+            const skip = page * limit;
+            const result = await productCollection.find().skip(skip).limit(limit).toArray();
             res.send(result);
         })
 
